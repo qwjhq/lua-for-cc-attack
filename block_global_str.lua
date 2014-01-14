018a3e2278522565756686fa87e9b9c116ee8760
qwjhq <qwjhq@126.com>
2014-01-14 21:59:08 +0800

    Create block_global_str.lua

---
+++ block_global_str.lua
@@ -0,0 +1,7 @@
+block_str_mem = ngx.shared.cc_dict
+white_ip={
+['192.168.111'] = '1', 
+['192.168.1'] = '1', 
+['10.1.1'] = '1' 
+}
+--white_uri={'/test1.php%?arg=10000007&style=1','/test2.php%?arg=10000007&style=1'}
